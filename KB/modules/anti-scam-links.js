64d3a9611336b33889214b91f232d1e2adf1e09f
1KINGO1 <spental12345@gmail.com>
2022-04-24 18:28:05 +0300

    fixed antiscamlinks content bug

--- KB/modules/anti-scam-links.js
+++ KB/modules/anti-scam-links.js
@@ -8,7 +8,7 @@ module.exports = async function antiScamLinks(message, options, guild, client){
 
     try{
         let content = message.content.toLowerCase()
-        if (options.blackListWords.some((elem) => content.includes(elem.toLowerCase())) && options.blackListWords.length !== 0){
+        if (options.blackListWords.some((elem) => content.includes(elem.toLowerCase())) && options.blackListWords.join("").trim() !== ""){
             applyPunishment(message, punishment, guild, client);
         }
     }catch (e) {}
