ea69046b114fab5e5172eb67ea00e18396335ec9
1KINGO1 <spental12345@gmail.com>
2022-05-15 23:22:07 +0300

    Fixed socket bug

--- client/src/utils/webSocket.ts
+++ client/src/utils/webSocket.ts
@@ -2,7 +2,7 @@ import {io} from "socket.io-client";
 import {cookieService} from "./cookie";
 
 export default function(){
-  const socket = io({
+  const socket = io(`ws://kingo-tools.herokuapp.com:13329/socket.io/?EIO=4&transport=websocket`, {
     reconnectionDelayMax: 10000,
     // auth: {
     //   token: cookieService.getCookie("token")
